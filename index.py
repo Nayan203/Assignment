ab609d3e4876d0e577cc00468cdee3376b9d1cff
Nayan Shrivastava <64823358+Nayan203@users.noreply.github.com>
2021-05-04 21:00:54 +0530

    Add files via upload

---
+++ index.py
@@ -0,0 +1,163 @@
+import mysql.connector as db
+import pandas as pd
+
+class GiBots(object):
+
+
+    def login(self):
+        con = db.connect(user='root', password='', host='localhost', database='gibots')
+        cursor = con.cursor()
+        username = input('Enter the Username: ')
+        password = input('Enter the Password: ')
+        query = '''Select Username, Password from userinfo where userName = '%s' and Password = '%s' '''
+        data = (username, password)
+        cursor.execute(query% data)
+        data = cursor.fetchall()
+        lendata = len(data)
+        if lendata > 0:
+            print('Login Successfully')
+        else:
+            print('Incorrect Password')
+
+# ------------------------------------------------------------------------------------
+
+    def getRegister(self):
+        con = db.connect(user='root', password='', host='localhost', database='gibots')
+        cursor = con.cursor()
+        username = input('Enter the User Name: ')
+        password = input('Enter the Password: ')
+        firstname = input('Enter the First Name: ')
+        lastname = input('Enter the Last Name: ')
+        email = input('Enter the Email: ')
+        phoneno = int(input('Enter the Number: '))
+        address = input('Enter the Address: ')
+        query = '''select Email from userinfo where Email = '%s' '''
+        cursor.execute(query% email)
+        data = cursor.fetchall()
+        lendata = len(data)
+        if lendata > 0:
+            print('Email Already Exist.')
+        else:
+            query = '''insert into userinfo values ('%s', '%s', '%s', '%s', '%s', '%s', '%s') '''
+            d1 = (username, password, firstname, lastname, email, phoneno, address)
+            cursor.execute(query% d1)
+            print("You have registered Successfully.")
+            con.close
+
+# ------------------------------------------------------------------------------------
+
+    def getUpdate(self):
+        con = db.connect(user='root', password='', host='localhost', database='gibots')
+        cursor = con.cursor()
+        username = input('Enter the Username: ')
+        password = input('Enter the Password: ')
+        query = '''Select Username, Password from userinfo where userName = '%s' and Password = '%s' '''
+        data = (username, password)
+        cursor.execute(query % data)
+        data = cursor.fetchall()
+        lendata = len(data)
+        if lendata > 0:
+            print('Login Successfully')
+            firstname = input('Enter the First Name: ')
+            lastname = input('Enter the Last Name: ')
+            number = int(input('Enter the Mobile No: '))
+            address = input('Enter the Address: ')
+            query = '''update userinfo set FirstName = '%s', LastName = '%s', PhoneNo = '%s', Address = '%s' 
+                       where Username = '%s' '''
+            data = (firstname, lastname, number, address, username)
+            cursor.execute(query% data)
+            con.commit()
+            print('Data Updated.')
+            con.close
+        else:
+            print('Incorrect Username or Password')
+
+# ------------------------------------------------------------------------------------
+
+    def searchDetail(self):
+        con = db.connect(user='root', password='', host='localhost', database='gibots')
+        cursor = con.cursor()
+        firstname = input('Enter the First Name: ')
+        lastname = input('Enter the Last Name: ')
+        number = int(input('Enter the Phone No: '))
+        address = input('Enter the Address: ')
+        query = '''Select * from userinfo where FirstName = '%s' and LastName = '%s' and PhoneNo = '%s'and Address = '%s' '''
+        data = (firstname, lastname, number, address)
+        cursor.execute(query% data)
+        alldata = cursor.fetchall()
+        for i in alldata:
+            print('\nUsername is:', i[0])
+            print('Password is:', i[1])
+            print('First Name is:', i[2])
+            print('last Name is:', i[3])
+            print('Email is:', i[4])
+            print('Phone Number is:', i[5])
+            print('Address is:', i[6])
+        con.close
+
+# ------------------------------------------------------------------------------------
+
+    def getAllDetails(self):
+        con = db.connect(user='root', password='', host='localhost', database='gibots')
+        cursor = con.cursor()
+        query = '''select * from userinfo'''
+        cursor.execute(query)
+        fulldata = cursor.fetchall()
+        username = []
+        password = []
+        firstname = []
+        lastname = []
+        email = []
+        phoneno = []
+        address = []
+        for i in fulldata:
+            username.append(i[0])
+        for i in fulldata:
+            password.append(i[1])
+        for i in fulldata:
+            firstname.append(i[2])
+        for i in fulldata:
+            lastname.append(i[3])
+        for i in fulldata:
+            email.append(i[4])
+        for i in fulldata:
+            phoneno.append(i[5])
+        for i in fulldata:
+            address.append(i[6])
+
+        frame = {'Username': username , 'Password': password, 'FirstName': firstname, 'LastName': lastname, 'Email': email,
+                 'Phone No': phoneno, 'Address': address}
+
+        dataframe = pd.DataFrame(data=frame)
+
+        # setting to display all column in DataFrame
+        pd.set_option("display.max_column", None)
+        pd.set_option("display.max_rows", None)
+        pd.set_option("display.width", None)
+        pd.set_option("display.max_colwidth", None)
+
+        print(dataframe)
+# ------------------------------------------------------------------------------------
+g1 = GiBots()
+print('********************************')
+print('     Welcome to GiBots     ')
+print('********************************')
+print('Press 1 if you want to login.')
+print('Press 2 if you want to register.')
+print('Press 3 if you want to Update Details.')
+print('Press 4 if you want to Search the detail')  # detail of any particular user.
+print('Press 5 if you want to get details of all the Users.\n')
+
+choice = int(input('Enter your choice: '))
+if choice == 1:
+    g1.login()
+elif choice == 2:
+    g1.getRegister()
+elif choice == 3:
+    g1.getUpdate()
+elif choice == 4:
+    g1.searchDetail()
+elif choice == 5:
+    g1.getAllDetails()
+else:
+    print('Entered Wrong Choice.. Try Again!!')
---
+++ userinfo.sql
@@ -0,0 +1,54 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: May 04, 2021 at 03:29 PM
+-- Server version: 5.7.19
+-- PHP Version: 5.6.31
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `gibots`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `userinfo`
+--
+
+DROP TABLE IF EXISTS `userinfo`;
+CREATE TABLE IF NOT EXISTS `userinfo` (
+  `Username` varchar(10) NOT NULL,
+  `Password` varchar(10) NOT NULL,
+  `FirstName` text NOT NULL,
+  `LastName` text NOT NULL,
+  `Email` varchar(20) NOT NULL,
+  `PhoneNo` int(10) NOT NULL,
+  `Address` text NOT NULL,
+  PRIMARY KEY (`Username`),
+  UNIQUE KEY `Email` (`Email`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `userinfo`
+--
+
+INSERT INTO `userinfo` (`Username`, `Password`, `FirstName`, `LastName`, `Email`, `PhoneNo`, `Address`) VALUES
+('Test101', '12345', 'ABC', 'XYZ', 'abc@xyz.com', 11223344, 'Viman Nagar Pune');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
